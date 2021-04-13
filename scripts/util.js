bd5fa7ba7e67047701c8a3c43d3b209e0d7d2e09
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-13 10:46:44 +0100

    Adjust screen height for morse board

--- scripts/util.js
+++ scripts/util.js
@@ -1,3 +1,13 @@
 export function getClientHeight () {
-  return document.body.clientHeight || window.innerHeight
+
+  /**
+   * Okay I know this seems kinda weird but there is some method
+   * to the madness
+   * 
+   * We multiply the height by 0.75 because we only want the game to take
+   * up the top 75% of the screen. This is because the 'morseboard' is set
+   * to 25vh which is 25%, so overall we end up with 100%
+   */
+
+  return (document.body.clientHeight || window.innerHeight) * 0.75 
 }
