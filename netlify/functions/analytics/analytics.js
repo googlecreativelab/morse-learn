a38dd48d31dc7a27ac477dad33b7e5014f0829b2
Gavin Henderson <github@gavinhenderson.co.uk>
2021-04-21 17:01:57 +0100

    Connect to local db

--- netlify/functions/analytics/analytics.js
+++ netlify/functions/analytics/analytics.js
@@ -1,17 +1,34 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+// get the client
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost:8889",
+  user: "root",
+  database: "morse_learn",
+  password: "root"
+});
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await new Promise((res, rej) => {
+      connection.query("SELECT ?", [1], function(err, results, fields) {
+        if(err) return rej(err) // results contains rows returned by server
+        res([results, fields])
+      }); 
+    }) 
+
+    console.log(response)
+
+    const subject = event.queryStringParameters.name || "World";
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
