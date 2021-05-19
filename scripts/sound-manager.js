644bebeafcbfef1dd95c173e40ab0845c84a95c4
Gavin Henderson <github@gavinhenderson.co.uk>
2021-05-19 09:12:57 +0100

    Switch to using howler

--- scripts/sound-manager.js
+++ scripts/sound-manager.js
@@ -1,34 +1,29 @@
+const { Howl } = require('howler');
+
 class SoundManager {
   constructor() {
     this.sounds = {};
   }
 
-  createSound(name, path, audioFormat = "audio/mp3") {
-    const newSound = document.createElement("audio");
-    var newSoundSource = document.createElement("source");
-    newSoundSource.setAttribute("src", path);
-    newSoundSource.setAttribute("type", audioFormat);
-    newSound.id = name;
-    newSound.style.position = "absolute";
-    newSound.style.visibility = "hidden";
-    newSound.appendChild(newSoundSource);
-    document.body.appendChild(newSound);
+  createSound(name, path) {
+    const newSound = new Howl({
+      src: [path]
+    });
 
     this.sounds[name] = newSound;
   }
 
   playSound(name) {
-    this.sounds[name].currentTime = 0;
     this.sounds[name].play();
   }
 
   soundDuration(name) {
-    const duration = this.sounds[name].duration;
+    const duration = this.sounds[name].duration();
     return duration;
   }
 
   stopSound(name) {
-    this.sounds[name].pause();
+    this.sounds[name].stop();
   }
 }
 
